955cd75ad5c765a2325551da127304cae2966669
ewen <ewen.grosjean@gmail.com>
2014-08-27 02:29:29 +0200

    immutant 2 and pedestal 0.3

--- test-server/ewen/cle_usb/test_server/test_server.clj
+++ test-server/ewen/cle_usb/test_server/test_server.clj
@@ -1,15 +1,15 @@
 (ns ewen.cle-usb.test-server.test-server
-  (:require [io.pedestal.service.http :as bootstrap]
-            [io.pedestal.service.http.route :as route]
-            [io.pedestal.service.http.body-params :as body-params]
-            [io.pedestal.service.http.route.definition :refer [defroutes]]
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.route.definition :refer [defroutes]]
             [ring.util.response :as ring-resp]
             [immutant.web :as web]
-            [immutant.dev :as immutant]
             [immutant.util]
-            [io.pedestal.service.http :as http]
             [cemerick.austin.repls :refer [browser-connected-repl-js]]
-            [net.cgrand.enlive-html :as enlive]))
+            [net.cgrand.enlive-html :as enlive]
+
+            [ring.middleware.resource]))
 
 
 
@@ -34,12 +34,38 @@
   (ring-resp/response (apply str (main-tml))))
 
 (defroutes routes
-           [[["/" {:get test-page} ^:interceptors [bootstrap/html-body]]]])
+           [[["/" {:get test-page} ^:interceptors [http/html-body]]]])
 
-(def service {::bootstrap/routes        #(deref #'routes)
-              ::bootstrap/resource-path "/public/"})
+(def service {::http/routes        #(deref #'routes)
+              ::http/resource-path "/public/"})
 
-#_(web/start-servlet "/" (::http/servlet (http/create-servlet service)))
-#_(cemerick.austin.repls/cljs-repl repl-env)
-#_(cemerick.austin.repls/exec)
-#_(web/stop)
+
+(comment
+  (web/run (::http/servlet (http/create-servlet service)) {:path "/"})
+  (cemerick.austin.repls/cljs-repl repl-env)
+  (cemerick.austin.repls/exec)
+  (web/stop {:path "/"})
+
+  (require '[ring.util.codec :as codec]
+   '[ring.util.response :as response]
+   '[ring.util.request :as request]
+   '[ring.middleware.head :as head])
+
+
+  (defn resource-request
+    "If request matches a static resource, returns it in a response map.
+    Otherwise returns nil."
+    {:added "1.2"}
+    [request root-path]
+    (if (#{:head :get} (:request-method request))
+      (let [path (codec/url-decode (request/path-info request))]
+        (-> (response/resource-response path {:root root-path})
+            (head/head-response request)))))
+
+  (alter-var-root #'ring.middleware.resource/resource-request
+                  (fn [_] (fn [request root-path]
+                            (resource-request request root-path))))
+
+
+
+  )
